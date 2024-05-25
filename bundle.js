7bd68b4fd88b590b7ddedbb08d765f73151a7d9d
jealarue <jesse.green81@jmail.com>
2024-05-25 08:29:22 -0600

    add/define React comonent, set initial state, and add comments on code blocks

--- bundle.js
+++ bundle.js
@@ -1,13 +1,50 @@
-!function() {
+!function () {
     "use strict";
+  
+    // Set options for the marked library
     marked.setOptions({
-        breaks: !0,
-        highlight: function(e) {
-            return Prism.highlight(e, Prism.languages.javascript, "javascript")
-        }
+      // Enable line breaks in the markdown
+      breaks: !0,
+  
+      // Define a function to highlight code using Prism.js
+      highlight: function (e) {
+        // Highlight the code as JavaScript using Prism.js
+        return Prism.highlight(e, Prism.languages.javascript, "javascript");
+      },
     });
-    const e = new marked.Renderer;
-    e.link = function(e, a) {
-     return '<a target="_blank" href="'.concat(e, '">').concat(a, "</a>")
+  
+    // Create a new instance of marked.Renderer to customize rendering
+    const e = new marked.Renderer();
+  
+    // Override the default link rendering to open links in a new tab
+    e.link = function (e, a) {
+      // Return the modified HTML for the link
+      return '<a target="_blank" href="'.concat(e, '">').concat(a, "</a>");
     };
- }
+  
+    // Define a React component
+    class t extends React.Component {
+      // Constructor to initialize the component
+      constructor(e) {
+        // Call the parent constructor with the props
+        super(e);
+  
+        // Set the initial state of the component
+        (this.state = {
+          // Initial markdown content
+          markdown: r,
+          // Flags to track if the editor or preview is maximized
+          editorMaximized: !1,
+          previewMaximized: !1,
+        }),
+  
+        // Bind the event handlers to the current instance
+        (this.handleChange = this.handleChange.bind(this)),
+        (this.handleEditorMaximize = this.handleEditorMaximize.bind(this)),
+        (this.handlePreviewMaximize = this.handlePreviewMaximize.bind(this));
+      }
+  
+      // Add your event handler methods (e.g., handleChange, handleEditorMaximize, handlePreviewMaximize) here
+    }
+  };
+  
