13101d8a320ddbad289be9ec43c2b700e7a320b1
jealarue <jesse.green81@jmail.com>
2024-05-25 09:40:57 -0600

    add/ create/render elements to return HTML

--- bundle.js
+++ bundle.js
@@ -55,7 +55,54 @@
         this.setState({ previewMaximized: !this.state.previewMaximized}) 
       }
         // Render the component next
+        render() {
+            // Determine the class names and icon based on the state
+            const e = this.state.editorMaximized 
+                ? ["editorWrap maximized", "previewWrap hide", "fa fa-compress"] 
+                : this.state.previewMaximized 
+                    ? ["editorWrap hide", "previewWrap maximized", "fa fa-compress"] 
+                    : ["editorWrap", "previewWrap", "fa fa-arrows-alt"];
+        
+            // Return the React elements
+            return React.createElement("div", null, 
+                // Editor section
+                React.createElement("div", {
+                    className: e[0] // Apply class name based on state
+                }, 
+                // Editor toolbar with maximize/minimize button
+                React.createElement(a, {
+                    icon: e[2], // Icon class name based on state
+                    onClick: this.handleEditorMaximize, // Event handler for maximizing/minimizing editor
+                    text: "Editor" // Button text
+                }), 
+                // Editor component
+                React.createElement(n, {
+                    markdown: this.state.markdown, // Pass markdown content as prop
+                    onChange: this.handleChange // Event handler for changes in the editor
+                })), 
                 
+                // Converter section (currently empty)
+                React.createElement("div", {
+                    className: "converter"
+                }), 
+                
+                // Previewer section
+                React.createElement("div", {
+                    className: e[1] // Apply class name based on state
+                }, 
+                // Previewer toolbar with maximize/minimize button
+                React.createElement(a, {
+                    icon: e[2], // Icon class name based on state
+                    onClick: this.handlePreviewMaximize, // Event handler for maximizing/minimizing previewer
+                    text: "Previewer" // Button text
+                }), 
+                // Previewer component
+                React.createElement(i, {
+                    markdown: this.state.markdown // Pass markdown content as prop
+                }))
+            );
+        }
+        
         
       
     }
